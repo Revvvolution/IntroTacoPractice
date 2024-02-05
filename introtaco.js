178db093cb0228b045c0e723418d09fdfde81fef
Revvvolution <huffman40@mctc.edu>
2024-02-04 23:43:34 -0500

    Finished Practice
    Finished chapter 1 practice.

---
+++ introtaco.js
@@ -0,0 +1,30 @@
+                            /* All About Me (Exercise) */
+
+let firstName = "Rev";
+let lastName = "Huffman";
+let age = 33;
+let likesTravel = true;
+
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+console.log(likesTravel);
+
+
+                            /* Practice */
+
+let temperature = 39;
+let conditions = "cloudy";
+let sunriseTime = "7:44am";
+let sunsetTime = "5:34pm";
+
+console.log(temperature);
+console.log(conditions);
+console.log(sunriseTime);
+console.log(sunsetTime);
+
+const todaysForecast = `It's currently ${conditions} and ${temperature}°F.
+The sun rose this morning at ${sunriseTime} and will set at ${sunsetTime}.`;
+
+console.log(todaysForecast);
+
